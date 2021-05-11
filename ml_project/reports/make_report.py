c0dd565eff67e296b4332e718d39bc9afeba89b3
bulaevvi <79763137+bulaevvi@users.noreply.github.com>
2021-05-11 16:03:25 +0500

    Update make_report.py

--- ml_project/reports/make_report.py
+++ ml_project/reports/make_report.py
@@ -1,11 +1,41 @@
 import pandas as pd
 from pandas_profiling import ProfileReport
+import logging.config
+import yaml
+import click
+from dataclasses import dataclass
+from marshmallow_dataclass import class_schema
 
 
-INPUT_FILE_NAME = 'data/raw/heart.csv'
-OUTPUT_FILE_NAME = 'reports/report.html'
+APPLICATION_NAME = 'homework01'
+logger = logging.getLogger(APPLICATION_NAME)
 
 
-data = pd.read_csv(INPUT_FILE_NAME)
-profile = ProfileReport(data)
-profile.to_file(output_file = OUTPUT_FILE_NAME)
+@dataclass
+class ReportParams:
+    input_data_path: str
+    output_data_path: str
+    logger_config: str
+   
+ReportParamsSchema = class_schema(ReportParams)
+
+
+@click.command(name="make_report")
+@click.argument("config_path")
+def make_report(config_path: str):
+    # Loading report params
+    with open(config_path, "r") as input_stream:
+        params = ReportParamsSchema().load(yaml.safe_load(input_stream))
+    # Loading logger params
+    with open(params.logger_config) as config_fin:
+        logging.config.dictConfig(yaml.safe_load(config_fin))
+    
+    logger.info("Start creating data report...")
+    input_csv_data = pd.read_csv(params.input_data_path)
+    profile = ProfileReport(input_csv_data)
+    profile.to_file(output_file = params.output_data_path)
+    logger.info("Data report has succesfully created")
+    
+
+if __name__ == "__main__":
+    make_report()
