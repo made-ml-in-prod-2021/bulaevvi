a537ac1a2c7014b1282d832f79c19f2dcc73f849
bulaevvi <79763137+bulaevvi@users.noreply.github.com>
2021-06-08 13:35:10 +0500

    Replaced e-mail in utils.py
    Указал фейковый адрес электронной почты, т.к. кто-то поднял мой код и шлет алерты на мой ящик

--- airflow_ml_dags/dags/utils.py
+++ airflow_ml_dags/dags/utils.py
@@ -4,7 +4,7 @@ VOLUME = 'C:/MADE/ML_in_prod/HW/airflow_ml_dags/data/:/data'
 
 default_args = {
     "owner": "Vladimir_Bulaev",
-    "email": ["bulaevvi@gmail.com"],
+    "email": ["my_mail@gmail.com"],
     "email_on_failure": True, # Alert в случае падения дага
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
